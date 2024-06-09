55de89b3e2f5480d0651991a8ffb2899f79bd324
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-09 12:41:51 +0200

    build(server): add missing clcc lib

---
+++ server/tests/libs/clcc/modules/unistd.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** clcc
+** File description:
+** unistd
+*/
+
+#pragma once
+
+#include "../includes/clcc.h"
+
+__CLCC_BEGIN_DECLS
+
+PUBLIC_PROTO_CLCC(int, isatty)
+PUBLIC_PROTO_CLCC(int, fork)
+
+__CLCC_END_DECLS
