bc027a1424121f48e6421f623b07d98c33e5d613
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-10 15:06:48 +0200

    feat(server): add server_t type and set of tests

--- server/includes/types/server.h
+++ server/includes/types/server.h
@@ -7,10 +7,14 @@
 
 #pragma once
 
+#include <netinet/in.h>
 #include "list.h"
 
 // @brief Set of file descriptors
 typedef fd_set fd_set_t;
+
+// @brief Internet address
+typedef struct sockaddr_in sockaddr_in_t;
 
 // @brief File descriptor states
 typedef struct fd_states_s {
@@ -28,6 +32,8 @@ typedef struct fd_states_s {
 typedef struct server_s {
     // @brief Server socket
     int socket;
+    // @brief Server address
+    sockaddr_in_t address;
     // @brief File descriptor to watch for
     fd_states_t fd_watch;
     // @brief Actual states of file descriptors that have
@@ -40,3 +46,27 @@ typedef struct server_s {
     // @brief List of shared events to send to several clients
     list_t *events;
 } server_t;
+
+/**
+ * @brief Initialize given server
+ * @param server Server to initialize
+ */
+void server_init(server_t *server);
+
+/**
+ * @brief Create a new server
+ * @return Created server or NULL if an error occurred
+ */
+server_t *server_new(void);
+
+/**
+ * @brief Free given server
+ * @param server Server to free
+ */
+void server_free(server_t *server);
+
+/**
+ * @brief Initialize given file descriptors states
+ * @param states File descriptors states to initialize
+ */
+void fd_states_init(fd_states_t *states);
