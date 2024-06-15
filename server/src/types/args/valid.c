4125e310a84d001b5853b4c3ceb2279174c5282e
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-15 13:19:13 +0200

    refactor(server): add check for illegal name GRAPHIC in teams names

--- server/src/types/args/valid.c
+++ server/src/types/args/valid.c
@@ -6,7 +6,22 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "types/args.h"
+
+static bool are_valid_teams(char **teams)
+{
+    size_t i = 0;
+
+    if (!teams)
+        return fprintf(stderr, "Missing teams names\n") && false;
+    while (teams[i]) {
+        if (strcmp(teams[i], "GRAPHIC") == 0)
+            return fprintf(stderr, "Illegal team name: GRAPHIC\n") && false;
+        i += 1;
+    }
+    return true;
+}
 
 bool args_are_valid(args_t *args)
 {
@@ -20,7 +35,7 @@ bool args_are_valid(args_t *args)
         return fprintf(stderr, "Missing map height\n") && false;
     if (args->clients_nb == 0)
         return fprintf(stderr, "Missing clients number\n") && false;
-    if (args->teams == NULL)
-        return fprintf(stderr, "Missing teams names\n") && false;
+    if (!are_valid_teams(args->teams))
+        return false;
     return true;
 }
