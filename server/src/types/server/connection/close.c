e0867c1baf0490e50b5dd4c5c039202d9b4d94ab
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-17 10:24:26 +0200

    feat(server): add util function to close all connections

--- server/src/types/server/connection/close.c
+++ server/src/types/server/connection/close.c
@@ -35,10 +35,33 @@ void server_close_connection(server_t *server, controller_t *controller)
     close(controller->generic.socket);
     log_info("Client on socket %d disconnected", controller->generic.socket);
     if (server) {
-        fd_states_unset(&server->fd_watch, controller->generic.socket,
+        fd_states_unset(&(server->fd_watch), controller->generic.socket,
             FD_STATES_R | FD_STATES_E | FD_STATES_W);
+        fd_states_unset(&(server->fd_actual), controller->generic.socket,
+            FD_STATES_R | FD_STATES_E | FD_STATES_W);
+        controller->generic.socket = -1;
         server_remove_controller(server, controller);
     } else {
         controller_free(controller);
     }
+}
+
+void server_close_all_connections(server_t *server)
+{
+    node_t *node = server->controllers->first;
+    controller_t *controller = NULL;
+
+    while (node) {
+        controller = NODE_TO_PTR(node, controller_t *);
+        if (!controller)
+            continue;
+        fd_states_unset((&server->fd_watch), controller->generic.socket,
+            FD_STATES_R | FD_STATES_E | FD_STATES_W);
+        fd_states_unset((&server->fd_actual), controller->generic.socket,
+            FD_STATES_R | FD_STATES_E | FD_STATES_W);
+        close(controller->generic.socket);
+        controller->generic.socket = -1;
+        node = node->next;
+    }
+    list_clear(server->controllers, &controller_free_as_node_data);
 }
