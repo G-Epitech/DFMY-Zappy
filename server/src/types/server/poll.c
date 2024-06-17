4085526282877f7d22cf78882d082ceca58d67a6
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-17 10:25:01 +0200

    feat(server): add poll server function

---
+++ server/src/types/server/poll.c
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2024
+** zappy_server
+** File description:
+** start.c
+*/
+
+#include <errno.h>
+#include <string.h>
+#include "log.h"
+#include "types/server.h"
+
+static void server_update_fd_watch_write(server_t *server)
+{
+    node_t *node = server->controllers->first;
+    controller_t *controller = NULL;
+
+    fd_states_clear(&server->fd_watch, FD_STATES_W);
+    while (node) {
+        controller = NODE_TO_PTR(node, controller_t *);
+        if (controller->generic.emissions->len > 0) {
+            fd_states_set(&server->fd_watch, controller->generic.socket,
+                FD_STATES_W
+            );
+        }
+        node = node->next;
+    }
+}
+
+int server_poll(server_t *server, timeval_t *timeout)
+{
+    fd_states_t *actual = &server->fd_actual;
+    int res = 0;
+
+    server_update_fd_watch_write(server);
+    *actual = server->fd_watch;
+    res = select(actual->max + 1, &actual->readable, &actual->writable,
+        &actual->except, timeout);
+    if (res == -1) {
+        if (errno != EINTR)
+            log_error("Server polling fail: %s", strerror(errno));
+        fd_states_clear(actual, FD_STATES_E | FD_STATES_W | FD_STATES_R);
+        return -1;
+    }
+    return res;
+}
