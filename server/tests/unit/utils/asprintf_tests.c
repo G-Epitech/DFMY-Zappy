70d60edd76a0089156bc39bdcf7c4d338135f75e
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-18 23:47:49 +0200

    refactor(server): add my_vasprintf function

--- server/tests/unit/utils/asprintf_tests.c
+++ server/tests/unit/utils/asprintf_tests.c
@@ -5,6 +5,7 @@
 ** asprintf_tests.c
 */
 
+#include <signal.h>
 #include <criterion/criterion.h>
 #include "utils.h"
 #include "clcc/modules/stdio.h"
@@ -14,9 +15,40 @@ Test(asprintf, test_asprintf)
 {
     char *str = NULL;
     int ret = my_asprintf(&str, "Hello, %s", "world");
-    cr_assert_eq(ret, 13);
+    cr_assert_eq(ret, 12);
     cr_assert_str_eq(str, "Hello, world");
     free(str);
+}
+
+Test(asprintf, test_asprintf_null)
+{
+    char *str = NULL;
+    int ret = my_asprintf(&str, NULL);
+    cr_assert_eq(ret, -1);
+    cr_assert_null(str);
+}
+
+Test(asprintf, test_vsnprintf_fail)
+{
+    char *str = NULL;
+    int ret = 0;
+
+    clcc_return_now(vsnprintf, -1);
+    ret = my_asprintf(&str, "Hello, %s", "world");
+    clcc_disable_control(vsnprintf);
+    cr_assert_eq(ret, -1);
+}
+
+Test(asprintf, test_vsnprintf_fail_on_second_call)
+{
+    char *str = NULL;
+    int ret = 0;
+
+    clcc_return_value_after(vsnprintf, -1, 1);
+    clcc_enable_control(vsnprintf);
+    ret = my_asprintf(&str, "Hello, %s", "world");
+    clcc_disable_control(vsnprintf);
+    cr_assert_eq(ret, -1);
 }
 
 Test(asprintf, test_malloc_fail)
@@ -24,8 +56,8 @@ Test(asprintf, test_malloc_fail)
     char *str = NULL;
     int ret = 0;
 
-    clcc_return_now(malloc, NULL);
+    clcc_return_now(calloc, NULL);
     ret = my_asprintf(&str, "Hello, %s", "world");
-    clcc_disable_control(malloc);
+    clcc_disable_control(calloc);
     cr_assert_eq(ret, -1);
 }
