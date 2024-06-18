ae806edc0a88385827f575a60b740faa17d9c3d8
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-18 12:45:54 +0200

    fix(server): fix smart ptr free policy that was not correct

--- server/src/types/smart_ptr/free.c
+++ server/src/types/smart_ptr/free.c
@@ -11,12 +11,12 @@ void smart_ptr_free(smart_ptr_t *smart_ptr)
 {
     if (!smart_ptr)
         return;
+    if (smart_ptr->ref_count > 0)
+        smart_ptr->ref_count -= 1;
     if (smart_ptr->ref_count == 0) {
         if (smart_ptr->destructor) {
             smart_ptr->destructor(smart_ptr->target);
         }
         free(smart_ptr);
-    } else {
-        smart_ptr->ref_count--;
     }
 }
