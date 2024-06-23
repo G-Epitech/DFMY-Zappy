a12e461100773870306d6446b913ff4d1004ba84
sdragos1 <dragos.suceveanu@epitech.eu>
2024-06-23 16:37:11 +0200

    fix(server): remove redundant list reset function

--- server/src/types/list/merge.c
+++ server/src/types/list/merge.c
@@ -8,25 +8,18 @@
 #include <stdlib.h>
 #include "types/list.h"
 
-static void list_reset(list_t *list)
-{
-    list->first = NULL;
-    list->last = NULL;
-    list->len = 0;
-}
-
 void list_merge(list_t *dest, list_t *src)
 {
     if (src->len == 0)
         return;
     if (dest->len == 0) {
         *dest = *src;
-        list_reset(src);
+        list_init(src);
         return;
     }
     dest->last->next = src->first;
     src->first->prev = dest->last;
     dest->last = src->last;
     dest->len += src->len;
-    list_reset(src);
+    list_init(src);
 }
