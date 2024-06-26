d4511fa41e1fdee7e3ea0f297d6b774cecf04adb
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-27 00:34:54 +0200

    feat(server): add game pause

--- server/src/app/world/lifecycle/events_delay.c
+++ server/src/app/world/lifecycle/events_delay.c
@@ -12,5 +12,6 @@ void app_handle_world_lifecycle_events_delay(world_t *world,
     __attribute__((unused)) server_t *server)
 {
     world_reset_event_delay(world);
-    world_update_resources_generation_delay(world);
+    if (!world->paused)
+        world_update_resources_generation_delay(world);
 }
